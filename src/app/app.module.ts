a9a31a2d92db2097005fc08fd145a12499aea66c
leoreisdias <leoanrdord99@gmail.com>
2020-10-07 12:02:16 -0300

    ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule
